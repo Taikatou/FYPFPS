6fc434ca403ae57ff4f5250c6c8b656976ceabc8
Taikatou <13123947@studentmail.ul.ie>
2017-01-31 15:25:42 +0000

    First Upload

---
+++ Source/FPSProject/FPSCharacter.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "FPSCharacter.generated.h"
+
+UCLASS()
+class FPSPROJECT_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSCharacter();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Handles input for moving forward and backward.
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	// Handles input for moving right and left.
+	UFUNCTION()
+		void MoveRight(float Value);
+
+	// Sets jump flag when key is pressed.
+	UFUNCTION()
+		void StartJump();
+
+	// Clears jump flag when key is released.
+	UFUNCTION()
+		void StopJump();
+
+	// FPS camera.
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* FPSCameraComponent;
+
+	// First-person mesh (arms), visible only to the owning player.
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* FPSMesh;
+
+
+	// Gun muzzle's offset from the camera location.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector MuzzleOffset;
+
+	// Projectile class to spawn.
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+		TSubclassOf<class AFPSProjectile> ProjectileClass;
+	
+	//health property
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float Health = 100;
+
+	//is dead property
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+		bool isDead = false;
+
+	//calculate death function (helper)
+	virtual void CalculateDead();
+
+	//calculate health function
+	UFUNCTION(BlueprintCallable, Category = "Base Character")
+		virtual void CalculateHealth(float delta);
+
+#if WITH_EDITOR
+	// Editor-centric code for changing properties
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+		override;
+#endif
+};
---
+++ Source/FPSProject/FPSHUD.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSProject.h"
+#include "FPSHUD.h"
+
+
+
+void AFPSHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	if (CrosshairTexture)
+	{
+		// Find the center of our canvas.
+		FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+		// Offset by half of the texture's dimensions so that the center of the texture aligns with the center of the Canvas.
+		FVector2D CrossHairDrawPosition(Center.X - (CrosshairTexture->GetSurfaceWidth() * 0.5f), Center.Y - (CrosshairTexture->GetSurfaceHeight() * 0.5f));
+
+		// Draw the crosshair at the centerpoint.
+		FCanvasTileItem TileItem(CrossHairDrawPosition, CrosshairTexture->Resource, FLinearColor::White);
+		TileItem.BlendMode = SE_BLEND_Translucent;
+		Canvas->DrawItem(TileItem);
+	}
+}
+
+void AFPSHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GEngine)
+	{
+		// Put up a debug message for five seconds. The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("We are using FPSHud."));
+	}
+}
